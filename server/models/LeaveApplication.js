6b03a193571e30ab129f6aa5f0fae68e966202e9
rinku-15 <balairinku21@gmail.com>
2026-07-17 06:23:46 +0530

    API created for leave application

---
+++ server/models/LeaveApplication.js
@@ -0,0 +1,17 @@
+import e from "express";
+import mongoose from "mongoose";
+
+const leaveApplicationScheme = new mongoose.Schema({
+    employeeId: {type: mongoose.Schema.types.ObjectId, ref: "Employee", required: true},
+    type: {type: String, enum: ["SICK", "CASUAL", "ANNUAL"], required: true},
+    startDate: {type: Date, required: true},
+    endDate: {type: Date, required: true},
+    reason: {type: String, required: true},
+    status: {type: String, enum: ["PENDING", "APPROVED", "REJECTED"], default: "PENDING"},
+
+}, {timestamps: true})
+
+const LeaveApplication = mongoose.models.
+LeaveApplication || mongoose.model("LeaveApplication", leaveApplicationScheme);
+
+export default LeaveApplication;
