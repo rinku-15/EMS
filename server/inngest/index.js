945d7ee77e040d14191c457112c41c70a329f2c0
rinku-15 <balairinku21@gmail.com>
2026-07-20 08:25:33 +0530

    fixed email send cron for attendance

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -163,10 +163,12 @@ const attendanceReminderCron = inngest.createFunction(
                         `
                     })
                 })
+                await Promise.all(emailPromises)
+                return {emailsSent: absentEmployees.length}
             })
         }
 
-        await Promise.all(emailPromises)
+        
         return { totalActive: activeEmployees.length, onLeave: onLeaveIds.length, checkedIn: checkedInIds.length, absent: absentEmployees.length }
     }
 );
