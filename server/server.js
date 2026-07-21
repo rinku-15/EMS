a6c8e45362f8f50702c8002d8065dfbf51fbd327
rinku-15 <balairinku21@gmail.com>
2026-07-21 14:08:02 +0530

    login fix

--- server/server.js
+++ server/server.js
@@ -17,7 +17,11 @@ const app = express()
 const PORT = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors())
+app.use(cors({
+  origin: "https://ems-beta-ochre.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(express.json())
 app.use(multer().none())
 
